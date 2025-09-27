57d15862d7536878b644789bdb9ce8eb159706fb
hrushikesh <81614137+gopi1234567890@users.noreply.github.com>
2025-09-27 17:05:59 -0500

    updated fornt and index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,7 +12,7 @@ import Voicetest from './pages/Voicetest';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ai-call-assistant">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/call" element={<Calllogs />} />
